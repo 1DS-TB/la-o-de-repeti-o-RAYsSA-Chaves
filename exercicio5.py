2b7e71dcad8e932d31e2d89cfaf8918c50ebdb7f
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-04-14 22:43:04 -0300

    Add files via upload

---
+++ exercicio5.py
@@ -0,0 +1,24 @@
+#Solicitar um número
+#verificar se ele é primo
+
+import re
+
+num = input("Digite um número: ")
+valido = bool(re.fullmatch(r"[-]?\d+", num))
+
+if valido == False:
+    print("INVALIDO")
+else:
+    num = int(num)
+    divisores = 0
+    if num <= 1:
+        print(f"{num} não é primo")
+    else: 
+        for i in range(2,num):
+            if num % i == 0:
+                divisores += 1
+        if divisores == 0:
+            print(f"{num} é primo")
+        else:
+            print(f"{num} não é primo")
+
